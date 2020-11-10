7b794c55475775f7fff144648bce9e6dfb9be1cc
zvestigo <zvestigo@icloud.com>
2020-11-10 13:23:44 -0500

    Added HTML elements and structure; Altered JS players to be created within teams

--- js/main.js
+++ js/main.js
@@ -1,72 +1,65 @@
-//Player and Stat Initializations
+//Team, Player & Stat Initializations
 
-
-function createPlayer(playerNumber, playerName, playerTeam) {
-    var player = {};
-    player.name = playerName;
-    player.number = playerNumber;
-    player.team = playerTeam;
-    
-    console.log(player.name + " is number " + player.number + " on " + player.team);
-    
-    player.points = 0;
-    player.fgAttempts = 0;
-    player.fgMade = 0;
-    player.fgPercentage = Math.round(player.fgMade / player.fgAttempts * 100) + "%";
-    player.threeAttempts = 0;
-    player.threeMade = 0;
-    player.threePercentage = Math.round(player.threeMade / player.threeAttempts * 100) + "%";
-    player.ftAttempts = 0;
-    player.ftMade = 0;
-    player.ftPercentage = Math.round(player.ftMade / player.ftAttempts * 100) + "%";
-    player.assists = 0;
-    player.defRebounds = 0;
-    player.offRebounds = 0;
-    player.totalRebounds = player.defRebounds + player.offRebounds;
-    player.blocks = 0;
-    player.steals = 0;
-    player.fouls = 0;
-    player.turnover = 0;
-    
-    return player;
+function createTeam (teamName) {
+  var team = {
+    name: teamName,
+    players: []
   }
   
-  var playerA1 = createPlayer(0, "Damian Lillard", "Team A");
-  var playerA2 = createPlayer(2, "PlayerA2", "Team A");
-  var playerA3 = createPlayer(3, "PlayerA3", "Team A");
-  var playerA4 = createPlayer(4, "PlayerA4", "Team A");
-  var playerA5 = createPlayer(5, "PlayerA5", "Team A");
-  var playerA6 = createPlayer(6, "PlayerA6", "Team A");
-  var playerA7 = createPlayer(7, "PlayerA7", "Team A");
-  var playerA8 = createPlayer(8, "PlayerA8", "Team A");
-  var playerA9 = createPlayer(9, "PlayerA9", "Team A");
-  var playerA10 = createPlayer(10, "PlayerA10", "Team A");
-  var playerA11 = createPlayer(11, "PlayerA11", "Team A");
-  var playerA12 = createPlayer(12, "PlayerA12", "Team A");
+  function createPlayer (playerNumber, playerName, indexNum) {
+    var player = {
+      name: playerName,
+      number: playerNumber,
+      
+      points: 0,
+      fgMade: 0,
+      fgAttempts: 0,
+      threeMade: 0,
+      threeAttempts: 0,
+      ftMade: 0,
+      ftAttempts: 0,
+      defRebounds: 0,
+      offRebounds: 0,
+      assists: 0,
+      blocks: 0,
+      steals: 0,
+      fouls: 0,
+      turnovers: 0
+    }; 
+    
+    console.log(player.name + " on " + team.name + " is number " + player.number);
+      
+    team.players[indexNum] = player;
+  }
   
-  var playerB1 = createPlayer(1, "PlayerB1", "Team B");
-  var playerB2 = createPlayer(2, "PlayerB2", "Team B");
-  var playerB3 = createPlayer(3, "PlayerB3", "Team B");
-  var playerB4 = createPlayer(4, "PlayerB4", "Team B");
-  var playerB5 = createPlayer(5, "PlayerB5", "Team B");
-  var playerB6 = createPlayer(6, "PlayerB6", "Team B");
-  var playerB7 = createPlayer(7, "PlayerB7", "Team B");
-  var playerB8 = createPlayer(8, "PlayerB8", "Team B");
-  var playerB9 = createPlayer(9, "PlayerB9", "Team B");
-  var playerB10 = createPlayer(10, "PlayerB10", "Team B");
-  var playerB11 = createPlayer(11, "PlayerB11", "Team B");
-  var playerB12 = createPlayer(12, "PlayerB12", "Team B");
+  createPlayer(0, "Damian Lillard", 0);
+  createPlayer(2, "Player2", 1);
+  createPlayer(3, "Player3", 2);
+  createPlayer(4, "Player4", 3);
+  createPlayer(5, "Player5", 4);
+  createPlayer(6, "Player6", 5);
+  createPlayer(7, "Player7", 6);
+  createPlayer(8, "Player8", 7);
+  createPlayer(9, "Player9", 8);
+  createPlayer(10, "Player10", 9);
+  createPlayer(11, "Player11", 10);
+  createPlayer(12, "Player12", 11);
   
-  
-  
-  
-  // Stat Actions
-  
-  function recordEvent(event, player) {
+  return team;
+}
+
+var teamA = createTeam("Team A");
+var teamB = createTeam("Team B");
+
+
+
+// Stat Actions
+
+function recordEvent(action, selectedPlayer) {
      
-     player = playerA1
-       
-     switch (event) {
+     player = selectedPlayer;
+  
+     switch (action) {
        case "2ptMade":
          player.points += 2;
          player.fgMade += 1;
@@ -115,13 +108,42 @@ function createPlayer(playerNumber, playerName, playerTeam) {
        case "turnover":
          player.turnovers += 1;
          break;
+      
   
          /* 
        case "team turnover":
-         break;  */
+         break; 
+         
+       Add in tallying team totals
+       Add in tallying shooting %s
+         */
          
      }
     
   }
-  
-  
+
+
+
+
+// ---------- TESTING ------------
+
+recordEvent("2ptMade", teamA.players[2]); //player3 makes 2pointer
+recordEvent("3ptMade", teamA.players[2]); //player3 makes 3pointer
+recordEvent("2ptMissed", teamA.players[2]); //player 3 misses 2pointer
+
+console.log(teamA.players[2].points); //returns 5
+console.log(teamA.players[2].fgMade); //returns 2
+console.log(teamA.players[2].fgAttempts); //return 3
+
+recordEvent("ftMade", teamA.players[2]); //player3 makes FT
+recordEvent("ftMissed", teamA.players[2]); //player3 misses FT
+recordEvent("ftMissed", teamA.players[2]); //player3 misses FT
+
+console.log(teamA.players[2].points); //returns 6
+console.log(teamA.players[2].ftMade); //returns 1
+console.log(teamA.players[2].ftAttempts); //return 3
+
+console.log("test8");
+
+
+
